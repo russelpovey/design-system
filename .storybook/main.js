bb358ff338614456bf6a762b62e3224edc348afa
russelpovey <russelglennpovey94@gmail.com>
2022-06-17 12:17:06 +0200

    Storybook deployment with GitHub action

--- .storybook/main.js
+++ .storybook/main.js
@@ -5,8 +5,7 @@ module.exports = {
     "@storybook/addon-essentials",
     "@storybook/addon-interactions",
     "@storybook/preset-create-react-app",
-    "@storybook/addon-interactions",
   ],
   framework: "@storybook/react",
-  staticDirs: ["../public"],
+  // staticDirs: ["../public"],
 };
